3242b3c3da746a06983fbefc62ef1f5faf5c4abc
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2024-09-10 21:08:04 -0500

    update index.js

--- index.js
+++ index.js
@@ -4,21 +4,27 @@ import * as dotenv from "dotenv";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import mongoose from "mongoose";
+import userRouter from "./routers/userRouter.js";
+import exerciseRouter from "./routers/exerciseRouter.js";
 
 const app = express();
 dotenv.config();
 
 app.use(cors());
-app.use(express.static("public"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.use("api/users", router);
+app.use("/api/users", userRouter);
+app.use("/api/users/:_id", exerciseRouter);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+const port = process.env.PORT || 3000;
 
 try {
   await mongoose.connect(process.env.MONGO_URI);
