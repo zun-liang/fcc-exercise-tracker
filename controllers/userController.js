111d6063ddfa4ad421f5b1daf67e6467d362e4c2
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2024-09-11 21:52:07 -0500

    add error handler

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,11 @@
 import User from "../models/UserModel.js";
 
 export const createUser = async (req, res) => {
-  const user = await User.create(req.body);
-  res.json({ username: user.username, _id: user._id });
+  try {
+    const user = await User.create(req.body);
+    res.json({ username: user.username, _id: user._id });
+  } catch (error) {
+    console.log(error);
+    res.json({ error });
+  }
 };
