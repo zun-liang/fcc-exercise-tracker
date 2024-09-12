280b73124a88e6c642c42ae2d5dc6c78f4713bb9
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2024-09-11 21:51:24 -0500

    update exercise model

--- models/ExerciseModel.js
+++ models/ExerciseModel.js
@@ -1,16 +1,23 @@
 import mongoose from "mongoose";
 
-const ExerciseSchema = new mongoose.Schema({
-  description: {
-    type: String,
-    required: true,
+const ExerciseSchema = new mongoose.Schema(
+  {
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: { type: Number, required: true },
+    date: { type: Date, required: true },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+    },
   },
-  duration: { type: Number, required: true },
-  date: { type: Date, required: true },
-  createdBy: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-  },
-});
+  { timestamps: true }
+);
 
 export default mongoose.model("Exercise", ExerciseSchema);
