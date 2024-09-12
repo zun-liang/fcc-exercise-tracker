b6829e7c06578d5ac4703f568e18afe67146db10
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2024-09-11 21:52:38 -0500

    update createExercise & getLogs

--- controllers/exerciseController.js
+++ controllers/exerciseController.js
@@ -1,12 +1,84 @@
 import Exercise from "../models/ExerciseModel.js";
+import User from "../models/UserModel.js";
 
 export const createExercise = async (req, res) => {
-  const exercise = await Exercise.create(req.body);
-  res.json({
-    _id: exercise._id,
-    username: exercise.createdBy,
-    date: exercise.date,
-    duration: exercise.duration,
-    description: exercise.description,
-  });
+  try {
+    const userId = req.body[":_id"];
+    const user = await User.findById(userId);
+    req.body.username = user.username;
+    req.body.createdBy = userId;
+    req.body.date = new Date(req.body.date);
+    const exercise = await Exercise.create(req.body);
+    const utcDateString = exercise.date
+      .toUTCString()
+      .replace(",", "")
+      .split(" ");
+    const utcWeekday = utcDateString[0];
+    const utcMonth = utcDateString[2];
+    const utcDay = utcDateString[1];
+    const utcYear = utcDateString[3];
+    const formattedDate = `${utcWeekday} ${utcMonth} ${utcDay} ${utcYear}`;
+    res.json({
+      _id: exercise._id,
+      username: exercise.username,
+      date: formattedDate,
+      duration: exercise.duration,
+      description: exercise.description,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({ error });
+  }
+};
+
+export const getLogs = async (req, res) => {
+  try {
+    const { from, to, limit } = req.query;
+    const userId = req.baseUrl.split("/")[3];
+    const queryObject = {
+      createdBy: userId,
+    };
+    if (from) {
+      queryObject.$and = [
+        {
+          date: { $gte: new Date(from) },
+        },
+      ];
+    }
+    if (to) {
+      queryObject.$and = [{ date: { $lte: new Date(to) } }];
+    }
+
+    let count = await Exercise.countDocuments(queryObject);
+    let exercises = await Exercise.find(queryObject).sort("createdAt");
+
+    if (limit) {
+      count = count > Number(limit) ? Number(limit) : count;
+      exercises = await Exercise.find(queryObject)
+        .sort("createdAt")
+        .limit(Number(limit));
+    }
+    const user = await User.findById(userId);
+    const logs = exercises.map((exercise) => {
+      const utcDateString = exercise.date
+        .toUTCString()
+        .replace(",", "")
+        .split(" ");
+      const utcWeekday = utcDateString[0];
+      const utcMonth = utcDateString[2];
+      const utcDay = utcDateString[1];
+      const utcYear = utcDateString[3];
+      const formattedDate = `${utcWeekday} ${utcMonth} ${utcDay} ${utcYear}`;
+      const simplifiedExercise = {
+        description: exercise.description,
+        duration: exercise.duration,
+        date: formattedDate,
+      };
+      return simplifiedExercise;
+    });
+    res.json({ _id: user._id, username: user.username, count, logs });
+  } catch (error) {
+    console.log(error);
+    res.json({ error });
+  }
 };
