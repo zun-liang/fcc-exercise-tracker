b0dd3a6991f97318c56d0af083308fb2d3c53cc1
Zun Liang <107293493+zun-liang@users.noreply.github.com>
2024-09-12 19:02:33 -0500

    add getUsers

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
-import { createUser } from "../controllers/userController.js";
+import { createUser, getUsers } from "../controllers/userController.js";
 const userRouter = Router();
 
-userRouter.route("/").post(createUser);
+userRouter.route("/").post(createUser).get(getUsers);
 
 export default userRouter;
